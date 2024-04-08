5003ef398df647bd0ca5786c6344fbf1874e8d8c
s364549 <s364549@oslomet.no>
2024-04-08 19:11:18 +0200

    Adding form

--- nav.js
+++ nav.js
@@ -1,6 +1,7 @@
 const primaryNav = document.querySelector('.primary-navigation');
-
 const navToggle = document.querySelector('.mobile-nav-toggle');
+
+const overlay = document.querySelector('#overlay');
 
 navToggle.addEventListener('click', () => {
   const visibility = primaryNav.getAttribute('data-visible');
@@ -8,8 +9,10 @@ navToggle.addEventListener('click', () => {
   if(visibility === 'false') {
     primaryNav.setAttribute('data-visible', true);
     navToggle.setAttribute('aria-expanded', true);
+    overlay.classList.remove('hidden');
   } else {
     primaryNav.setAttribute('data-visible', false);
     navToggle.setAttribute('aria-expanded', false);
+    overlay.classList.add('hidden');
   }
 });
